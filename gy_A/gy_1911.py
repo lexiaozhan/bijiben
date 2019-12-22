094589425b7cbfbafd2640c4b051b91e48ffaf32
lexiaozhan <email@example.com>
2019-12-22 13:32:46 +0800

    first commit

---
+++ gy_A/gy_1911.py
@@ -0,0 +1,14 @@
+''' "asajsa"
+b ="sdddddd"
+print(a+b)'''
+try:
+    os.mkdir("gy_C")
+except Exception:
+    print("文件夹已存在")
+else:
+    print("文件夹创建成功")
+finally:
+    print("创建文件夹结束")
+
+print("sdfsdf")
+
